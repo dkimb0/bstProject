10dad4c87445241508ccdfa748b61efe516be939
dkimb0 <jhdavidk@gmail.com>
2024-01-02 15:56:58 -0500

    set up delete except for 2 child, create identification methods for nodes, tree find method

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 class Node {
-    constructor(){
-        this.value = null;
+    constructor(value){
+        this.value = value;
         this.left = null;
         this.right = null;
     }
+
+    isLeafNode(){
+        return this.left === null && this.right === null;
+    }
+
+    hasOneChild(){
+        return Boolean(this.left) !== Boolean(this.right);
+    }
+
+    hasTwoChild(){
+        return Boolean(this.left) && Boolean(this.right);
+    }
+
+
 }
 
 class Tree {
@@ -12,11 +26,32 @@ class Tree {
         this.root = null;
     }
 
+    find(value){
+        let tmp = this.root;
+        let tmpParent = {};
+        while (!tmp.isLeafNode()){
+            if (value === tmp.value){
+                return [tmp, tmpParent];
+            }
+            
+            tmpParent = tmp;
+            if (value < tmp.value){
+                tmp = tmp.left;
+            }else if (value > tmp.value){
+                tmp = tmp.right;
+            }
+
+        }
+        console.log('value not found');
+        return null;
+
+    }
+
     insert(value) {
         // traverse through tree, looking for last leaf (while left/right is not null)
         let tmp = this.root;
     
-        while(tmp.value !== null){
+        while(!tmp.isLeafNode()){
             if (value < tmp.value){
                 tmp = tmp.left;
             }else{
@@ -24,22 +59,59 @@ class Tree {
             }
         }
 
-        //set up new leaf node
-        tmp.value = value;
-        tmp.left = new Node;
-        tmp.right = new Node;
+
+        //create new node, assign value
+        if (value < tmp.value){
+            tmp.left = new Node(value);
+        }else{
+            tmp.right = new Node(value);
+        }
+    
+    }
+
+    delete(value){
+        let [deleteNode, parentNode] = this.find(value);
+        
+        console.log(deleteNode);
+        console.log(parentNode);
+
+        //if value is not in tree do nothing
+        if (!deleteNode){
+            return;
+            //if node to delete is a leaf node, remove from parent
+        }else if (deleteNode.isLeafNode()){
+            console.log('element is leaf node');
+            if (parentNode.left === deleteNode){
+                parentNode.left = null;
+            }else{
+                parentNode.right = null;
+            }
+        }else if (deleteNode.hasOneChild()){
+            console.log('element has one child');
+            if (parentNode.left === deleteNode){
+                parentNode.left = (deleteNode.left || deleteNode.right);                
+            }
+        }else{
+            console.log('element has two children');
+            let tmp = deleteNode.right;
+            while(tmp.left !== null){
+                tmp = tmp.left;
+            }
+            console.log(tmp);
+        }
+
+
+
+
+
+
     }
 }
 
-
-
-// [DONE] takes input array
-// [DONE] sorts 
-// [DONE] removes duplicates
-// [DONE] turns array into balanced binary tree of Node objects
-// [DONE] returns level-0 root node
+// sorts and remove dupes of input array
+// turns array into balanced binary tree of Node objects
+// returns level-0 root node
 // is there any way to put this in the tree class?
-// set up buildTree as a node func
 function buildTree(array){
     //init new root node
     let rootNode = new Node;
@@ -48,13 +120,13 @@ function buildTree(array){
     // add new nodes to make insert easier
     if (array.length === 1){
         rootNode.value = array[0];
-        rootNode.left = new Node;
-        rootNode.right = new Node
+        // rootNode.left = new Node;
+        // rootNode.right = new Node
         return rootNode;
     }
 
     if (array.length < 1){
-        return rootNode;
+        return null;
     }
 
     //sort array
@@ -94,9 +166,6 @@ function buildTree(array){
 
 }
 
-
-
-
 const prettyPrint = (node, prefix = "", isLeft = true) => {
     // console.log(node);
     // console.log(node.right);
@@ -118,9 +187,13 @@ let userArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 //create tree
 let userTree = new Tree(userArray);
 //set tree root to output of build tree (0th level root)
-
 userTree.root = buildTree(userTree.array);
-prettyPrint(userTree.root);
 
-// userTree.insert(3650);
+
+userTree.insert(3650);
+prettyPrint(userTree.root);
 // userTree.insert(3652);
+// userTree.delete(3);
+// console.log(userTree.find(7));
+userTree.delete(8);
+prettyPrint(userTree.root);
